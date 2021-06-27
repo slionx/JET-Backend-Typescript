87aa12aaa2310cd1569246b392c1da595a590e3b
shock <fastestmanonmars@gmail.com>
2021-06-27 15:44:04 +0200

    Fix Player.All

--- src/objects/entities/player.entity.ts
+++ src/objects/entities/player.entity.ts
@@ -3,7 +3,9 @@ import { IEntity } from "./interfaces/entity.interface";
 
 export class Player extends BaseObject implements IEntity {
     private static _all: { [key: string]: Player } = {};
-    static readonly all = Object.values(Player._all);
+    static all() {
+        return Object.values(Player._all);
+    }
 
     constructor() {
         super(ObjectType.Player);
